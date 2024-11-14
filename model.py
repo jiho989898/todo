b28dab5d69084d0b7bdbb80d98b4fe504160fdae
jiho <jiho@test.com>
2024-11-14 16:53:19 +0900

    응답모델 구성완료

--- model.py
+++ model.py
@@ -1,4 +1,5 @@
 from pydantic import BaseModel
+from typing import List
 
 class Todo(BaseModel):
     id: int
@@ -22,4 +23,21 @@ class todoItem(BaseModel):
                 "example": {
                     "item": "변경할 아이템 작성"
                 }
+        }
+
+#todo_list의 item만 리턴하기 위한 모델
+class TodoItems(BaseModel):
+    todos: List[todoItem]
+    class Config:
+        json_schema_extra = {
+            "example": {
+                "todos": [
+                    {
+                        "item": "첫번재 아이템"
+                    },
+                    {
+                        "item": "두번재 아이템"
+                    }
+                ]
+            }
         }
--- todo.py
+++ todo.py
@@ -1,20 +1,24 @@
-from fastapi import APIRouter, Path
-from model import Todo, todoItem
+from fastapi import APIRouter, Path, HTTPException,status
+from model import Todo, todoItem, TodoItems
 
 todo_router = APIRouter()
 todo_list = []
 
-@todo_router.post("/todo")
+@todo_router.post("/todo", status_code=201)
 async def add_todo(todo:Todo) -> dict:
     todo_list.append(todo)
     return {
         "message ": "todo added successfully"
     }
+
+
+
 @todo_router.get("/todo")
-async def retrieve_todos() ->dict:
+async def retrieve_todos() -> dict:
     return {
         "todo" : todo_list
     }
+
 @todo_router.get("/todo/{todo_id}")
 async def get_single_todo(todo_id: int = Path(..., title="특정 todo를 확인하기 위한 ID",ge=1,le=1000)) -> dict:
     for todo in todo_list:
@@ -22,9 +26,10 @@ async def get_single_todo(todo_id: int = Path(..., title="특정 todo를 확인�
             return {
                 "todo": todo
             }
-    return{
-        "message": "does not exist"
-    }
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail="존재하지 않는 ID 입니다."
+    )
 
 # 개별 ip의 item을 수정하기(put)
 @todo_router.put("/todo/{todo_id}")
@@ -35,9 +40,10 @@ async def update_todo(todo_data: todoItem, todo_id: int = Path(..., title="변�
             return {
                 "message": "todo가 업데이트 되었습니다."
             }
-    return{
-        "message": "does not exist"
-    }
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail="존재하지 않는 ID 입니다."
+    )
 
 @todo_router.delete("/todo")
 async def delete_todo() -> dict:
@@ -55,6 +61,7 @@ async def delete_single_todo(todo_id: int) -> dict:
         if todo.id == todo_id:
             todo_list.pop(index)
             return { "message": "삭제되었습니다"}
-    return {
-        "message": "존재하지 않는 id 입니다"
-    }
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail="존재하지 않는 ID 입니다."
+    )
